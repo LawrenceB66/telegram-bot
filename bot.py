ea4d62ac9eb3a631c4f18f0c6d17f3534c290ce4
LawrenceB66 <lawrencebucher@hotmail.com>
2026-05-06 11:58:54 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 
 # --- CONFIG ---
 TOKEN = os.getenv("TOKEN")
-FMP_API_KEY = os.getenv("FMP_API_KEY")
+ALPHA_API_KEY = os.getenv("ALPHA_API_KEY")
 
 CHANNEL_ID = -1003667470993
 TG_URL = f"https://api.telegram.org/bot{TOKEN}/"
