9a969e572c1152ba2d0752d9cc4327d3c229b10e
LawrenceB66 <lawrencebucher@hotmail.com>
2026-08-07 09:14:27 -0700

    Update watchlist.py

--- watchlist.py
+++ watchlist.py
@@ -1,134 +1,73 @@
 # ==========================================================
 # IT AIN'T LUCK LLC MASTER DISCOVERY UNIVERSE
 #
-# This file contains the complete ticker universe monitored
-# by the IAL Product Engine.
+# Complete A-Z ticker universe monitored by the
+# IAL Product Engine.
 #
 # bot.py imports TICKERS directly from this file.
-#
 # ==========================================================
 
 TICKERS = [
-
-    # ======================================================
-    # INDEXES | ETFs
-    # ======================================================
-
-    "DIA",  "IWM",  "QQQ",  "SMH", "SOXX", "SPY",  "TLT",  "UVXY", "VIXY", "VOO",  "XLE",  "XLF", "XLI",  "XLK",  "XLP",  "XLRE", "XLU",  "XLV",  "XLY",  "XME",
-
-    # ======================================================
-    # MEGA CAP TECHNOLOGY
-    # ======================================================
-
-    "ABBV", "ABT", "ACN", "ADP", "AIG", "AMGN", "AMT", "AXP", "BA", "BAC", "BABA", "BBVA", "BDX", "BK", "BKNG", "BLK", "BMY", "BN", "BRK-B", "BSX", "C", "CAT", "CB", "CCEP", "CDNS", 
-    "CHTR", "CI", "CL", "CMCSA", "CME", "COF", "COP", "COST", "CSX", "CVS", "CVX", "DASH", "DE", "DHR", "DIS", "DUK", "ECL", "ELV", "EMR", "ETN", "FDX", "FI", "GE", "GILD", "GM", "GS", 
-    "HD", "HON", "ICE", "ISRG", "JNJ", "JPM", "KHC", "KO", "LIN", "LLY", "LMT", "LOW", "MA", "MCD", "MDT", "META", "MMM", "MO", "MRK", "MS", "NEE", "NFLX", "NKE" "PFE", "PG", "PGR", "PLD", 
-    "PM", "PSX", "REGN", "RTX", "SBUX", "SCHW", "SHOP", "SO", "SPGI", "SPLK", "SRE", "SYK", "T", "TGT", "TMO", "TMUS", "TSLA", "UNH", "UNP", "UPS", "USB", "V", "VLO", "VRTX", "VZ", "WFC", 
-    "WMT", "XOM",
-
-    # ======================================================
-    # SEMICONDUCTORS
-    # ======================================================
-
-    "ACMR", "ADI",  "ALAB", "AMD", "AMAT", "AMKR", "ARM",  "ASML", "ASX", "AVGO", "AXTI", "CEVA", "COHR", "CRDO", "FORM", "GFS", "INDO", 
-    "INTC", "KLAC", "LSCC", "LRCX", "MCHP", "MPWR", "MRVL", "MTSI", "MU", "NVDA", "NXPI", "ON", "PLAB", "QCOM", "RMBS", "SIMO", "SITM", 
-    "SMTC", "STM", "SWKS", "TER", "TSM", "TXN", "UMC", "VECO",
-
-    # ======================================================
-    # SOFTWARE | AI
-    # ======================================================
-
-    "ADBE", "ADSK", "AI", "AMZN", "ANET", "CRM",  "CRWD", "CSCO", "DDOG", "DELL", "GOOG", "GOOGL", "HPE", "HPQ", "IBM", "INTU", "MSFT", 
-    "NOW", "ORCL", "PLTR", "SAP", "SNOW", "TEAM", "UBER", "VRT", "WDC", "ZM",
-
-    # ======================================================
-    # CYBERSECURITY
-    # ======================================================
-
-    "AKAM", "ALTR", "BAND", "CHKP", "CSGS", "CYBR", "FTNT", "GEN", "HACK", "NET", "OKTA", "PANW", "RNG", "S", "SAIC", "SOUN", "SPSC", "TENB", "TTWO", "VRSN", "VRNS", "ZS",
-
-    # ======================================================
-    # COMMUNICATION SERVICES
-    # ======================================================
-
-    "EA", "FOX", "FOXA", "LYV", "MTCH", "PARA", "PINS", "RBLX", "ROKU", "SNAP", "SPOT", "WBD",
-
-    # ======================================================
-    # ELECTRIC VEHICLES
-    # ======================================================
-
-    "CHPT", "FSR", "LCID", "LI", "NIO", "RIVN", "WKHS", "XPEV",
-
-    # ======================================================
-    # AUTO MANUFACTURERS
-    # ======================================================
-
-    "F", "HMC", "RACE", "STLA", "TM",
-
-    # ======================================================
-    # BATTERIES / LITHIUM
-    # ======================================================
-
-    "ALB", "ENVX", "FLNC", "FREY", "LAC", "PLL", "QS", "SLDP",
-
-    # ======================================================
-    # CRYPTO / BLOCKCHAIN
-    # ======================================================
-
-    "BITF", "BTBT", "CIFR", "CLSK", "COIN", "CORZ", "HIVE", "HOOD", "HUT", "IREN", "MARA", "RIOT", "WULF",
-
-    # ======================================================
-    # FINANCIALS
-    # ======================================================
-
-    "BEN", "CFG", "CMA", "DFS", "FITB", "HBAN", "KEY", "MKTX", "MTB", "PNC", "PYPL", "RF", "SOFI", "STT", "SYF", "TFC",
-
-    # ======================================================
-    # INSURANCE
-    # ======================================================
-
-    "AFL", "ALL", "CINF", "EG", "ERIE", "GL", "HIG", "LNC", "MET", "PRU", "RGA", "TRV", "UNM", "WRB",
-
-    # ======================================================
-    # HEALTHCARE
-    # ======================================================
-
-    "BAX", "BIIB", "CAH", "COR", "DXCM", "GEHC", "HCA", "HOLX", "HUM", "IDXX", "ILMN", "MRNA", "RMD", "STE", "ZBH", "ZTS",
-
-    # ======================================================
-    # BIOTECHNOLOGY
-    # ======================================================
-
-    "ACAD", "ALNY", "ARWR", "BEAM", "BLUE", "CRBU", "CRSP", "DNA", "EDIT", "EXAS", "EXEL", "FATE", "IONS", "NTLA", "RXRX", "SANA", "SRPT",
-    "TWST", "VERV",
-
-    # ======================================================
-    # INDUSTRIALS
-    # ======================================================
-
-    "CARR", "CMI", "CTAS", "FAST", "GNRC", "HWM", "IR", "JCI", "LHX", "NDSN", "NOC", "OTIS", "PH", "PCAR", "PWR", "ROK", "TT", "TXT", "URI", "WM", "XYL",
-
-    # ======================================================
-    # WEBULL PENNY STOCK
-    # ======================================================
-
-    "AAGIY", "ACGBY", "ADDYY", "ADRNY", "ADYEY", "AHEXY", "AJNMY", "AKZOY", "ALFVY", "ALIZY", "ALSMY", "AMADY", "AMKBY", "ANGPY", "ASMPT", 
-    "ASMVY", "ASXFY", "ATEYY", "AUO", "AUOTY", "AXAHY", "BACHY", "BAESY", "BAYRY", "BBSEY", "BDORY", "BHKLY", "BKHYY", "BKKLY", "BKRKY", 
-    "BNPQY", "BNTGY", "BRDCY", "BURBY", "BVVBY", "BYDDY", "CABGY", "CAJPY", "CCOEY", "CFRUY", "CGEMY", "CHEOY", "CHGCY", "CICHY", "CIHKY", 
-    "CKHUY", "CLLNY", "CODYY", "CRHKY", "CRRFY", "CRZBY", "CSLLY", "CSUAY", "CTTAY", "CZMWY", "DASTY", "DBOEY", "DBSDY", "DIDIY", "DIFTY", 
-    "DLAKY", "DNBBY", "DNNGY", "DSCSY", "DSDVY", "DTEGY", "DUFRY", "EADSY", "EBKDY", "ELEZY", "ENLAY", "ESAIY", "ESLOY", "ESYJY", "EVVTY", 
-    "EXPGY", "FANUY", "FJTSY", "FMCC", "FNMA", "FRCOY", "FSNUY", "FSUGY", "FUJHY", "FUJIY", "GBOOY", "GLNCY", "GMVHY", "GVDNY", "GXYYY", 
-    "HCMLY", "HEINY", "HENKY", "HESAY", "HKHHY", "HKXCY", "HLPPY", "HOCPY", "HSQVY", "HXGBY", "IBDRY", "IDEXY", "IFNNY", "IHICY", "IMBBY", 
-    "IMPUY", "ISNPY", "JAPSY", "JBAXY", "JMHLY", "JMPLY", "JSAIY", "KBCSY", "KCDMY", "KHNGY", "KKPNY", "KMTUY", "KNRRY", "KPCPY", "KRYAY",
-    "KYOAY", "LGGNY", "LGRDY", "LKNCY", "LNNGY", "LNVGY", "LRLCY", "LSRCY", "LTGHY", "LVMUY", "LZAGY", "MAKSY", "MARUY", "MBGYY", "MGDDY", 
-    "MHGVY", "MIELY", "MITEY", "MITSY", "MKKGY", "MPNGY", "MRAAY", "MSADY", "MTNOY", "MTSFY", "MTUAY", "MZDAY", "NABZY", "NCBDY", "NDBKY", 
-    "NEXOY", "NGLOY", "NHYDY", "NINOY"  "NLST",  "NNGRY", "NPSCY", "NPSNY", "NRDBY", "NSANY", "NSRGY", "NTDOY", "NTOIY", "NTTYY", "NVZMY", 
-    "OMRNY", "ORKLY", "OTGLY", "OTSKY", "PNGAY", "PPERY", "PPRUY", "PRNDY", "PROSY", "PRYMY", "PTAIY", "PUBGY", "QABSY", "RANJY", "RBGLY", 
-    "RDEIY", "REPYY", "RMVEY", "RNECY", "RNMBY", "RWEOY", "RYCEY", "SAFRY", "SALRY", "SAUHY", "SAXPY", "SBGSY", "SCGLY", "SCMWY", "SDVKY", 
-    "SDXAY", "SEKEY", "SEOAY", "SFTBY", "SGAPY", "SGIOY", "SGPYY", "SGSOY", "SHECY", "SIEGY", "SINGY", "SKBSY", "SKFRY", "SLLDY", "SMERY",
-    "SMGZY", "SMMNY", "SMMYY", "SMNNY", "SMPNY", "SMTOY", "SNMRY", "SOUHY", "SPKKY", "SSDOY", "SSEZY", "SSMXY", "SSUMY", "STRNY", "SUHJY", 
-    "SVNDY", "SVNLY", "SWDBY", "SWGAY", "SWRAY", "SXYAY", "SYIEY", "TCEHY", "TEZNY", "TGOPY", "TKAMY", "TKOMY", "TLPFY", "TLSNY", "TMVWY",
-    "TOELY", "TRUMY", "TRYIY", "TTDKY", "TTNDY", "UBSFY", "UCBJY", "UNCRY", "UPMMY", "VCISY", "VDMCY", "VEOEY", "VIVHY", "VLEEY", "VLVLY", 
-    "VWAGY", "VWDRY", "WEGRY", "WEGZY", "WFAFY", "WMMVY", "WTBDY", "XIACY", "XNGSY", "YAHOY", "YARIY", "YASKY", "ZLNDY", "ZURVY",
+    "AAGIY", "ABBV", "ABT", "ACAD", "ACGBY", "ACMR", "ACN", "ADBE", "ADDYY", "ADI",
+    "ADP", "ADRNY", "ADSK", "ADYEY", "AFL", "AHEXY", "AI", "AIG", "AJNMY", "AKAM",
+    "AKZOY", "ALAB", "ALB", "ALFVY", "ALIZY", "ALL", "ALNY", "ALSMY", "ALTR", "AMADY",
+    "AMAT", "AMD", "AMGN", "AMKBY", "AMKR", "AMT", "AMZN", "ANET", "ANGPY", "ARM",
+    "ARWR", "ASMPT", "ASML", "ASMVY", "ASX", "ASXFY", "ATEYY", "AUO", "AUOTY", "AVGO",
+    "AXAHY", "AXP", "AXTI", "BA", "BABA", "BACHY", "BAC", "BAESY", "BAND", "BAX",
+    "BAYRY", "BBSEY", "BBVA", "BDORY", "BDX", "BEAM", "BEN", "BHKLY", "BKHYY", "BIIB",
+    "BITF", "BK", "BKKLY", "BKNG", "BKRKY", "BLK", "BLUE", "BMY", "BN", "BNPQY",
+    "BNTGY", "BRDCY", "BRK-B", "BSX", "BTBT", "BURBY", "BVVBY", "BYDDY", "C", "CABGY",
+    "CAH", "CAJPY", "CARR", "CAT", "CB", "CCEP", "CCOEY", "CDNS", "CEVA", "CFG",
+    "CFRUY", "CGEMY", "CHEOY", "CHGCY", "CHKP", "CHPT", "CHTR", "CI", "CICHY", "CIFR",
+    "CIHKY", "CINF", "CKHUY", "CL", "CLLNY", "CLSK", "CMA", "CMCSA", "CME", "CMI",
+    "CODYY", "COF", "COHR", "COIN", "COP", "COR", "CORZ", "COST", "CRBU", "CRDO",
+    "CRHKY", "CRM", "CRRFY", "CRSP", "CRWD", "CRZBY", "CSCO", "CSGS", "CSLLY", "CSUAY",
+    "CSX", "CTAS", "CTTAY", "CVS", "CVX", "CYBR", "CZMWY", "DASH", "DASTY", "DBOEY",
+    "DBSDY", "DDOG", "DE", "DELL", "DFS", "DHR", "DIA", "DIDIY", "DIFTY", "DIS",
+    "DLAKY", "DNA", "DNBBY", "DNNGY", "DSCSY", "DSDVY", "DTEGY", "DUFRY", "DUK", "DXCM",
+    "EA", "EADSY", "EBKDY", "ECL", "EDIT", "EG", "ELEZY", "ELV", "EMR", "ENLAY",
+    "ENVX", "ERIE", "ESAIY", "ESLOY", "ESYJY", "ETN", "EVVTY", "EXAS", "EXEL", "EXPGY",
+    "F", "FANUY", "FAST", "FATE", "FDX", "FI", "FITB", "FJTSY", "FLNC", "FMCC",
+    "FNMA", "FORM", "FOX", "FOXA", "FRCOY", "FREY", "FSNUY", "FSR", "FSUGY", "FTNT",
+    "FUJHY", "FUJIY", "GBOOY", "GE", "GEHC", "GEN", "GFS", "GILD", "GL", "GLNCY",
+    "GM", "GMVHY", "GNRC", "GOOG", "GOOGL", "GS", "GVDNY", "GXYYY", "HACK", "HBAN",
+    "HCA", "HCMLY", "HD", "HEINY", "HENKY", "HESAY", "HIG", "HIVE", "HKHHY", "HKXCY",
+    "HLPPY", "HMC", "HOCPY", "HOLX", "HON", "HOOD", "HPE", "HPQ", "HSQVY", "HUM",
+    "HUT", "HWM", "HXGBY", "IBM", "IBDRY", "ICE", "IDEXY", "IDXX", "IFNNY", "IHICY",
+    "ILMN", "IMBBY", "IMPUY", "INDO", "INTC", "INTU", "IONS", "IR", "IREN", "ISNPY",
+    "ISRG", "IWM", "JAPSY", "JBAXY", "JCI", "JMHLY", "JMPLY", "JNJ", "JPM", "JSAIY",
+    "KBCSY", "KCDMY", "KEY", "KHC", "KHNGY", "KKPNY", "KLAC", "KMTUY", "KNRRY", "KO",
+    "KPCPY", "KRYAY", "KYOAY", "LAC", "LCID", "LGGNY", "LGRDY", "LHX", "LI", "LIN",
+    "LKNCY", "LLY", "LMT", "LNC", "LNNGY", "LNVGY", "LOW", "LRLCY", "LSCC", "LSRCY",
+    "LTGHY", "LRCX", "LVMUY", "LYV", "LZAGY", "MA", "MAKSY", "MARA", "MARUY", "MBGYY",
+    "MCD", "MCHP", "MDT", "MET", "META", "MGDDY", "MHGVY", "MIELY", "MITEY", "MITSY",
+    "MKKGY", "MKTX", "MMM", "MO", "MPNGY", "MPWR", "MRAAY", "MRK", "MRNA", "MRVL",
+    "MS", "MSADY", "MSFT", "MTB", "MTCH", "MTNOY", "MTSFY", "MTSI", "MTUAY", "MU",
+    "MZDAY", "NABZY", "NCBDY", "NDBKY", "NDSN", "NEE", "NET", "NEXOY", "NFLX", "NGLOY",
+    "NHYDY", "NINOY", "NIO", "NKE", "NLST", "NNGRY", "NOC", "NOW", "NPSCY", "NPSNY",
+    "NRDBY", "NSANY", "NSRGY", "NTDOY", "NTLA", "NTOIY", "NTTYY", "NVDA", "NVZMY", "NXPI",
+    "OKTA", "OMRNY", "ON", "ORCL", "ORKLY", "OTGLY", "OTIS", "OTSKY", "PANW", "PARA",
+    "PCAR", "PFE", "PG", "PGR", "PH", "PINS", "PLAB", "PLD", "PLL", "PLTR",
+    "PM", "PNC", "PNGAY", "PPERY", "PPRUY", "PRNDY", "PROSY", "PRU", "PRYMY", "PSX",
+    "PTAIY", "PUBGY", "PWR", "PYPL", "QABSY", "QCOM", "QQQ", "QS", "RACE", "RANJY",
+    "RBGLY", "RBLX", "RDEIY", "REGN", "REPYY", "RF", "RGA", "RIOT", "RIVN", "RMBS",
+    "RMD", "RMVEY", "RNECY", "RNG", "RNMBY", "ROK", "ROKU", "RTX", "RWEOY", "RXRX",
+    "RYCEY", "S", "SAFRY", "SAIC", "SALRY", "SANA", "SAP", "SAUHY", "SAXPY", "SBGSY",
+    "SBUX", "SCGLY", "SCHW", "SCMWY", "SDVKY", "SDXAY", "SEKEY", "SEOAY", "SFTBY", "SGAPY",
+    "SGIOY", "SGPYY", "SGSOY", "SHECY", "SHOP", "SIEGY", "SIMO", "SINGY", "SITM", "SKBSY",
+    "SKFRY", "SLDP", "SLLDY", "SMERY", "SMGZY", "SMH", "SMMNY", "SMMYY", "SMNNY", "SMPNY",
+    "SMTC", "SMTOY", "SNAP", "SNMRY", "SNOW", "SO", "SOFI", "SOUHY", "SOUN", "SOXX",
+    "SPGI", "SPKKY", "SPLK", "SPOT", "SPSC", "SPY", "SRE", "SRPT", "SSDOY", "SSEZY",
+    "SSMXY", "SSUMY", "STE", "STLA", "STM", "STRNY", "STT", "SUHJY", "SVNDY", "SVNLY",
+    "SWDBY", "SWGAY", "SWKS", "SWRAY", "SYF", "SYIEY", "SYK", "SXYAY", "T", "TCEHY",
+    "TEAM", "TENB", "TER", "TEZNY", "TFC", "TGT", "TGOPY", "TKAMY", "TKOMY", "TLPFY",
+    "TLSNY", "TLT", "TM", "TMO", "TMUS", "TMVWY", "TOELY", "TRUMY", "TRYIY", "TSLA",
+    "TSM", "TT", "TTDKY", "TTNDY", "TTWO", "TWST", "TXN", "TXT", "UBER", "UBSFY",
+    "UCBJY", "UMC", "UNCRY", "UNH", "UNM", "UNP", "UPMMY", "UPS", "URI", "USB",
+    "UVXY", "V", "VCISY", "VDMCY", "VECO", "VEOEY", "VERV", "VIVHY", "VIXY", "VLEEY",
+    "VLO", "VLVLY", "VOO", "VRSN", "VRNS", "VRT", "VRTX", "VWAGY", "VWDRY", "VZ",
+    "WBD", "WDC", "WEGRY", "WEGZY", "WFAFY", "WFC", "WKHS", "WM", "WMMVY", "WMT",
+    "WRB", "WTBDY", "WULF", "XIACY", "XLE", "XLF", "XLI", "XLK", "XLP", "XLRE",
+    "XLU", "XLV", "XLY", "XME", "XNGSY", "XOM", "XPEV", "XYL", "YAHOY", "YARIY",
+    "YASKY", "ZBH", "ZLNDY", "ZM", "ZS", "ZTS", "ZURVY",
 ]
-
